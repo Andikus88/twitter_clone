2ec993c5ab02e8cdee6e9882e301a340067187c0
andi kuswoyo <andiq2810@gmail.com>
2023-07-15 02:38:16 +0700

    Setting up Flutter Project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databaseId = '64326c4b699c90087328';
+  static const String projectId = '6429378e1c390999dab1';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1/';
+  }
+
